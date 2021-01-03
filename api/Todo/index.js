93bf6a220885ab06be207d0804f8817efe159343
anh duy <anhduy.260798@gmail.com>
2021-01-03 21:41:57 +0700

    update code

--- api/Todo/index.js
+++ api/Todo/index.js
@@ -1,21 +1,21 @@
 export const TodoAPI = {
   GetAll(axios, userid) {
-    return axios.get("/api/Task/GetAllTask?userId=" + userid).then(resp => {
+    return axios.get("/api/Task?userId=" + userid).then(resp => {
       return resp;
     });
   },
   AddTodo(axios, model) {
-    return axios.post("/api/Task/AddTask", model).then(resp => {
+    return axios.post("/api/Task", model).then(resp => {
       return resp;
     });
   },
   DoneTodo(axios, taskId) {
-    return axios.put("/api/Task/DoneTask?taskid=" + taskId).then(resp => {
+    return axios.put("/api/Task?taskid=" + taskId).then(resp => {
       return resp;
     });
   },
   DeleteTodo(axios, taskId) {
-    return axios.delete("/api/Task/DeleteTask?taskid=" + taskId).then(resp => {
+    return axios.delete("/api/Task?taskid=" + taskId).then(resp => {
       return resp;
     });
   }
--- api/User/index.js
+++ api/User/index.js
@@ -1,6 +1,6 @@
 export const UserAPI = {
   UpdateUser(axios, model) {
-    return axios.post("/api/Users/UpdateInfo", model).then(resp => {
+    return axios.post("/api/Users/UpdateUser", model).then(resp => {
       return resp;
     });
   }
